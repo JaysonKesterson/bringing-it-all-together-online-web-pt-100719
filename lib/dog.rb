8ac06b76d41e44af5ac6f92b190a2721099056f3
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-04 15:15:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,7 +66,8 @@ class Dog
   end
   
   def self.find_or_create_by(name:,breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed")
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed ='#{breed}'")
+    
   end
   
   
